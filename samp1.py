05af77f3360853fbfd0b25627830aff9ab6ddf12
vinodjsr <63946065+vinodjsr@users.noreply.github.com>
2021-09-29 14:48:39 +0530

    Create samp1.py

---
+++ samp1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import datetime
+import pytz
+import requests
+import logging
+from pdfExtraction import *
+import os
+import re
+
+lxq=[1,2,3,4,'abc','efg']
+for i in range(len(lxq)):
+  print('printing elements of list ',lxq[i])
+  
